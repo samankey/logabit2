584c1329ddd768e056ca59d9d726f5ba3471d641
samankey <71953671+samankey@users.noreply.github.com>
2022-02-28 22:15:16 +0900

    WIP: 린트 정리중

--- pages/join/index.vue
+++ pages/join/index.vue
@@ -8,6 +8,7 @@
     <Input v-model="password" :label="'비밀번호'" :type="'password'" :placeholder="'비밀번호를 입력해주세요! (최소 8글자 이상)'"/>
     <Input v-model="passwordCheck" :label="'비밀번호 확인'" :type="'password'" :placeholder="'비밀번호를 다시 입력해주세요!'"/>
     <Input v-model="name" :label="'닉네임'" :placeholder="'5글자 이하 한글 닉네임만 가능해요!'"/>
+    {{ checkEmail }}
     <ButtonDefault :text="'가입을 완료하시겠어요?'" :style-type="'join'" />
     <ButtonDefault :text="'이미 계정이 있으신가요?'" />
   </div>
@@ -37,7 +38,10 @@ export default {
   },
 
   computed: {
-    
+    checkEmail () {
+      const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+      return regEmail.test(this.email);
+    },
   }
 }
 </script>
